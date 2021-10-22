bd41836805765ad5f5c687fb87f8e297e8676599
Aaron Hennessey <axelancerr@gmail.com>
2021-10-22 02:47:22 +0100

    update snowflake.py

--- cdn/utilities/snowflake.py
+++ cdn/utilities/snowflake.py
@@ -4,6 +4,9 @@ from __future__ import annotations
 # Standard Library
 import time
 
+# Packages
+import pendulum
+
 # My stuff
 from core import values
 
@@ -18,19 +21,15 @@ data_center_id_shift = sequence_bits + worker_id_bits
 timestamp_left_shift = sequence_bits + worker_id_bits + data_center_id_bits
 
 
-def snowflake_to_timestamp(snowflake: int) -> int:
+def snowflake_to_datetime(id: int) -> pendulum.DateTime:
 
-    snowflake >>= 22
-    snowflake += values.EPOCH
-    snowflake /= 1000
-    return snowflake
+    timestamp = ((id >> 22) + values.EPOCH) / 1000
+    return pendulum.from_timestamp(timestamp, tz="UTC")
 
 
-def generate_snowflake(worker_id: int, data_center_id: int) -> int:
+def generate_snowflake(worker_id: int = 1, data_center_id: int = 1) -> int:
 
     assert 0 <= worker_id <= max_worker_id
     assert 0 <= data_center_id <= max_data_center_id
 
-    timestamp = round(time.time()*1000)
-
-    return ((timestamp - values.EPOCH) << timestamp_left_shift) | (data_center_id << data_center_id_shift) | (worker_id << worker_id_shift)
+    return ((round(time.time() * 1000) - values.EPOCH) << timestamp_left_shift) | (data_center_id << data_center_id_shift) | (worker_id << worker_id_shift)
